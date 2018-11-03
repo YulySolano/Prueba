eac13ffcd0f63bd7c072700b2cf63d9de9ccbf37
Yuly Solano <yuly0911@gmail.com>
2018-11-03 18:15:06 -0500

    ejercicio 2

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
-
 const Hapi = require('hapi');
-
 const Inert = require('inert');
-
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'videoclub',
+    password: 'yulyss',
+    port: 5432
+});
 const server = new Hapi.Server({
     port: 3000,
     host: "localhost",
     routes: {
         files: {
-            relativeTo: './public'       
-         }
+            relativeTo: './public'
+        }
     }
 });
 
@@ -31,8 +36,28 @@ const init = async () => {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-    
             return 'Hello, world!';
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/socios',
+        handler: async (request, h) => {
+            let result = await pool.query ('SELECT * FROM socios');
+            return result.rows;
+        }
+    });
+    server.route({
+        method: 'POST',
+        path: '/socios',
+        handler: async (request, h) => {
+            try {
+                let body = request.payload;
+                let result = await pool.query ('INSERT INTO socios VALUES($1, $2, $3, $4)',[body.codigo,body.nombre,body.direccion,body.telefono]);
+                return result.rows;
+            } catch (error) {
+                console.log(error)
+            }
         }
     });
     await server.start();
